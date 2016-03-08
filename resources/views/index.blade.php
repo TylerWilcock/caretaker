67dfb972cb0cadd166a357c99bc362a938715900
Kyle Amundson <amundsonkc06@uww.edu>
2016-03-07 20:47:55 -0600

    Finished updating all the references to the resources on the index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,13 +12,13 @@
     <title>Landing Page - Start Bootstrap Theme</title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="<?php echo asset('assets/css/bootstrap.css') ?>" rel="stylesheet">
+    <link href="{{asset('assets/css/bootstrap.css')}}" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="<?php echo asset('assets/css/landing-page.css') ?>" rel="stylesheet">
+    <link href="{{asset('assets/css/landing-page.css')}}" rel="stylesheet">
 
     <!-- Custom Fonts -->
-    <link href="<?php echo asset('font-awesome') ?>" rel="stylesheet" type="text/css">
+    <link href="{{asset('assets/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
     <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -111,7 +111,7 @@
                     <p class="lead">A special thanks to <a target="_blank" href="http://join.deathtothestockphoto.com/">Death to the Stock Photo</a> for providing the photographs that you see in this template. Visit their website to become a member.</p>
                 </div>
                 <div class="col-lg-5 col-lg-offset-2 col-sm-6">
-                    <img class="img-responsive" src="img/ipad.png" alt="">
+                    <img class="img-responsive" src="{{asset('assets/img/ipad.png')}}" alt="">
                 </div>
             </div>
 
@@ -133,7 +133,7 @@
                     <p class="lead">Turn your 2D designs into high quality, 3D product shots in seconds using free Photoshop actions by <a target="_blank" href="http://www.psdcovers.com/">PSDCovers</a>! Visit their website to download some of their awesome, free photoshop actions!</p>
                 </div>
                 <div class="col-lg-5 col-sm-pull-6  col-sm-6">
-                    <img class="img-responsive" src="img/dog.png" alt="">
+                    <img class="img-responsive" src="{{asset('assets/img/dog.png')}}" alt="">
                 </div>
             </div>
 
@@ -155,7 +155,7 @@
                     <p class="lead">This template features the 'Lato' font, part of the <a target="_blank" href="http://www.google.com/fonts">Google Web Font library</a>, as well as <a target="_blank" href="http://fontawesome.io">icons from Font Awesome</a>.</p>
                 </div>
                 <div class="col-lg-5 col-lg-offset-2 col-sm-6">
-                    <img class="img-responsive" src="img/phones.png" alt="">
+                    <img class="img-responsive" src="{{asset('assets/img/phones.png')}}" alt="">
                 </div>
             </div>
 
@@ -224,10 +224,10 @@
     </footer>
 
     <!-- jQuery -->
-    <script src="js/jquery.js"></script>
+    <script src="{{asset('assets/js/jquery.js')}}"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
 
 </body>
 
