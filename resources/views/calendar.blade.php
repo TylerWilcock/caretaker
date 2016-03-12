137cf100b20d219099642e127e3cdcc2380188ad
Tydiggidy <lindsayt10@uww.edu>
2016-03-11 22:33:11 -0600

    Minor changes
    Gets the user who is logged in. Not just John Doe.
    Changed titles to Care Teammate

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -8,7 +8,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>Gentallela Alela! | </title>
+  <title>Care Teammate | </title>
 
   <!-- Bootstrap core CSS -->
 
@@ -48,7 +48,7 @@
         <div class="left_col scroll-view">
 
           <div class="navbar nav_title" style="border: 0;">
-            <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>Gentellela Alela!</span></a>
+            <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>Care Teammate</span></a>
           </div>
           <div class="clearfix"></div>
 
@@ -60,7 +60,7 @@
             </div>
             <div class="profile_info">
               <span>Welcome,</span>
-              <h2>John Doe</h2>
+              <h2>{{Auth::user()}}</h2>
             </div>
           </div>
           <!-- /menu prile quick info -->
