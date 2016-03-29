e4679b2a5c01a864cf803e9d1725cafc9f76a483
Tydiggidy <lindsayt10@uww.edu>
2016-03-29 14:25:13 -0500

    Added username
    Toolbar will show the users name instead of John Doe

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -97,7 +97,7 @@
             <ul class="nav navbar-nav navbar-right">
               <li class="">
                 <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                  <img src="{{asset('assets/img/img.jpg')}}" alt="">John Doe
+                  <img src="{{asset('assets/img/img.jpg')}}" alt="">{{Auth::user()->name}}
                   <span class=" fa fa-angle-down"></span>
                 </a>
                 <ul class="dropdown-menu dropdown-usermenu animated fadeInDown pull-right">
