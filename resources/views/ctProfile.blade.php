ac9d8d97b65c25d0a91bda484be5649b9abc0bdd
Kyle Amundson <amundsonkc06@uww.edu>
2016-03-31 14:12:59 -0500

    Added user name to profile page

--- resources/views/ctProfile.blade.php
+++ resources/views/ctProfile.blade.php
@@ -61,7 +61,7 @@
               <img src="{{asset('assets/img/img.jpg')}}" alt="..." class="img-circle profile_img">
             </div>
             <div class="profile_info">
-              <span>Welcome, *Auth::user()->name didn't work here*</span>
+              <span>Welcome, {{$caretakerInfo->full_name}}</span>
             </div>
           </div>
           <!-- /menu prile quick info -->
@@ -96,7 +96,7 @@
             <ul class="nav navbar-nav navbar-right">
               <li class="">
                 <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                  <img src="{{asset('assets/img/img.jpg')}}" alt="">John Doe
+                  <img src="{{asset('assets/img/img.jpg')}}" alt="">{{$caretakerInfo->full_name}}
                   <span class=" fa fa-angle-down"></span>
                 </a>
                 <ul class="dropdown-menu dropdown-usermenu animated fadeInDown pull-right">
