7fd4e397b3e20f63cb149b6105e822157cb68d92
Kyle Amundson <amundsonkc06@uww.edu>
2016-04-07 09:11:47 -0500

    Working on form for adding CR

--- resources/views/ctProfile.blade.php
+++ resources/views/ctProfile.blade.php
@@ -39,6 +39,11 @@
     color:#00c;
     text-decoration:underline; 
   }
+
+  .crTable>tbody>tr>td { 
+    vertical-align: middle; 
+  }
+
 </style>
 
 
@@ -211,9 +216,83 @@
                                 @endif
                             </tbody>
                         </table>
+                        <button type="button" class="btn btn-primary" id = "addCr">Add Care Recipient</button>
                     </div>
                 </div>
             </div>
+          </div>
+          <div class="row">
+              <div class="col-md-12 col-sm-12 col-xs-12">
+                  <div class="x_panel">
+                      <div class="x_title">
+                          <h2>Add Care Recipient <small>Enter Care Recipient's Information</small></h2>
+                          <div class="clearfix"></div>
+                      </div>
+                      <!-- o Client name, birthdate, home address, phone number, emergency contact, and notes, doctor(s) contact information -->
+                      <div class="x_content">
+                          <br>
+                          <form id="crForm" class="form-horizontal form-label-left">
+
+                              <div class="form-group">
+                                  <label class="control-label col-md-3 col-sm-3 col-xs-12">Name <span class="required">*</span>
+                                  </label>
+                                  <div class="col-md-6 col-sm-6 col-xs-12">
+                                      <input type="text" id="crName" required="required" class="form-control col-md-7 col-xs-12">
+                                  </div>
+                              </div>
+                              <div class="form-group">
+                                  <label class="control-label col-md-3 col-sm-3 col-xs-12">Birthdate <span class="required">*</span>
+                                  </label>
+                                  <div class="col-md-6 col-sm-6 col-xs-12">
+                                      <input id="birthday" class="date-picker form-control col-md-7 col-xs-12" required="required" type="text">
+                                  </div>
+                              </div>
+                              <div class="form-group">
+                                  <label class="control-label col-md-3 col-sm-3 col-xs-12">Home Address <span class="required">*</span>
+                                  </label>
+                                  <div class="col-md-6 col-sm-6 col-xs-12">
+                                      <input type="text" id="crHomeAddress" required="required" class="form-control col-md-7 col-xs-12">
+                                  </div>
+                              </div>
+                              <div class="form-group">
+                                  <label class="control-label col-md-3 col-sm-3 col-xs-12">Phone Number <span class="required">*</span>
+                                  </label>
+                                  <div class="col-md-6 col-sm-6 col-xs-12">
+                                      <input type="text" id="crPhoneNumber" required="required" class="form-control col-md-7 col-xs-12">
+                                  </div>
+                              </div>
+                              <div class="form-group">
+                                  <label class="control-label col-md-3 col-sm-3 col-xs-12">Emergency Contact
+                                  </label>
+                                  <div class="col-md-6 col-sm-6 col-xs-12">
+                                      <input type="text" id="crEmergencyContact" class="form-control col-md-7 col-xs-12">
+                                  </div>
+                              </div>
+                              <div class="form-group">
+                                  <label class="control-label col-md-3 col-sm-3 col-xs-12">Doctor's Contact <span class="required">*</span>
+                                  </label>
+                                  <div class="col-md-6 col-sm-6 col-xs-12">
+                                      <input type="text" id="crDrContact" required="required" class="form-control col-md-7 col-xs-12">
+                                  </div>
+                              </div>
+                              <div class="form-group">
+                                  <label class="control-label col-md-3 col-sm-3 col-xs-12">Notes
+                                  </label>
+                                  <div class="col-md-6 col-sm-6 col-xs-12">
+                                      <input type="textarea" id="crNotes" class="form-control col-md-7 col-xs-12">
+                                  </div>
+                              </div>
+                              <div class="ln_solid"></div>
+                              <div class="form-group">
+                                  <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+                                      <button type="submit" class="btn btn-primary">Add</button>
+                                  </div>
+                              </div>
+
+                          </form>
+                      </div>
+                  </div>
+              </div>
           </div>
         </div>
 
@@ -497,6 +576,10 @@
       $('.recipient').click(function(){
         var crID = $(this).data("id");
         window.location.href  = "/cr/profile/" + crID;
+      });
+
+      $('#addCr').click(function(){
+        alert('hello');
       });
 
     });
