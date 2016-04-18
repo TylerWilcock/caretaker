c791f9180fb0d09a332d0c61cb3a04ef4ff41fe1
jrupp68 <ruppjj29@uww.edu>
2016-04-18 17:40:07 -0500

    Fixed User Registration Fields
    This commit of basically one line took over 6 hours of sad, sad life

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,7 +14,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-     'first_name', 'last_name', 'birthday', 'address', 'phone', 'emergency_phone', 'email', 'password'
+        'first_name', 'last_name', 'birthday', 'address', 'phone', 'emergency_phone', 'email', 'password'
     ];
 
     /**
